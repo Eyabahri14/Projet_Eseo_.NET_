c07c774dafb4322e54485634e0eb0aba3b7bace1
mathildegreen <122628430+mathildegreen@users.noreply.github.com>
2024-01-05 16:53:12 +0100

    Event Delegate Evenement

--- SW/SW/SW.Models/Citoyen.cs
+++ SW/SW/SW.Models/Citoyen.cs
@@ -1,6 +1,8 @@
 ﻿namespace SW.Models
 {
+    
     public class Citoyen
+
     {
         public int Id { get; set; }
         public string Nom { get; set; }
@@ -18,5 +20,53 @@
         public int? Fertilite { get; set; }
         public int? PointsDeMerites { get; set; }
 
+        static private void MethodeUse()
+        {
+            Evenement CitoyenEvent = new Evenement();
+
+            Citoyen citoyen = new Citoyen();
+
+            CitoyenEvent.MyEvent += new DelegueEvenement(citoyen.EvenementSurBonheur);
+
+            CitoyenEvent.MyEvent += new DelegueEvenement(citoyen.EvenementSurFertilite);
+
+            CitoyenEvent.MyEvent += new DelegueEvenement(citoyen.EvenementSurPointsDeMerites);
+
+            //Pour l'instant on déclenche l'événement ici
+            CitoyenEvent.LancerEvenement();
+        }
+
+        public void EvenementSurBonheur(string s)
+        {
+            Random aleatoire = new Random();
+
+            int alea = aleatoire.Next(100);
+            
+            Bonheur = Bonheur - alea;
+
+
+        }
+
+        public void EvenementSurFertilite(string s)
+        {
+            Random aleatoire = new Random();
+
+            int alea = aleatoire.Next(100);
+
+            Fertilite = Fertilite - (int)(alea/4);
+
+
+        }
+
+        public void EvenementSurPointsDeMerites(string s)
+        {
+            Random aleatoire = new Random();
+
+            int alea = aleatoire.Next(100);
+
+            PointsDeMerites = PointsDeMerites - (int)(alea / 8);
+
+
+        }
     }
 }
